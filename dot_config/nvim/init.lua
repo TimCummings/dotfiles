3cd3b71f0d74cae782564af2a58aab71b5b7fdb3
Tim Cummings <timjc86@gmail.com>
2021-07-01 13:59:23 -0500

    Revert "feat: vim - bootstrap package manager"
    This reverts commit 0468f9bec9acb9768603958cd29763ec6135b818.

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -44,15 +44,6 @@ o.smartcase = true
 -- highlight current cursor line
 wo.cursorline = true
 
--- bootstrap package manager (Packer)
-local execute = vim.api.nvim.command
-local fn = vim.fn
-local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
-if fn.empty(fn.glob(install_path)) > 0 then
-  fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
-  execute 'packadd packer.nvim'
-end
-
 -- colorscheme
 -- require('nord').set()
 -- vim.g.onedark_style = 'dark'
