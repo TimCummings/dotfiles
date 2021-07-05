4d9312e94e350ccf96ac1242381ce222a98ca373
Tim Cummings <timjc86@gmail.com>
2021-07-05 11:08:22 -0500

    fix: vim - LSP keybindings
    - remove unnecessary language server `setup` call
    - unnecessary call was being called first and wasn't passing the
      `on_attach` function, so keybindings weren't being set

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,6 +1,5 @@
 -- Install packer
 local execute = vim.api.nvim_command
-
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
@@ -88,7 +87,6 @@ if pcall(function()
     require('colorizer').setup()
     require('statusline')
     require('lsp')
-    require('lspconfig').tsserver.setup{}
     require('treesitter_config').setup()
     -- installing parsers currently locks up VPS
     -- manually install parsers one at a time for now with `:TSInstall`
