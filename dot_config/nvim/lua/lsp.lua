7486984117b9210d6461f3854d6e45346814a72b
Tim Cummings <timjc86@gmail.com>
2021-11-13 16:45:16 -0600

    refactor: vim - remove enter LSP Confirm mapping
    - retain previous behavior (prior to switching from nvim-compe to
      nvim-cmp) of `<CR>` inserting a normal carriage return
    - just commenting out for now since I might want this back when
      exploring formatting/linting via LSP

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -75,10 +75,10 @@ cmp.setup({
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete(),
     ['<C-e>'] = cmp.mapping.close(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
+    -- ['<CR>'] = cmp.mapping.confirm {
+    --   behavior = cmp.ConfirmBehavior.Replace,
+    --   select = true,
+    -- },
     ['<Tab>'] = function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
