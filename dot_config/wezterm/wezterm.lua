86a17e5ffd335b30d02b9457c9f41fe457afacd9
Tim Cummings <timjc86@gmail.com>
2021-11-15 16:38:55 -0600

    style: terminal - improve cursor color
    - note: wezterm themes override all color settings; to apply any color
      setting, you must completely disable the theme and manually apply all
      other theme colors

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,13 +1,29 @@
 return {
   -- color_scheme = 'Ayu Mirage',
+  -- color_scheme = 'Batman',
   -- color_scheme = 'BlulocoDark',
-  color_scheme = 'ChallengerDeep',
+  -- color_scheme = 'ChallengerDeep',
   -- color_scheme = 'Darkside',
   -- color_scheme = 'FirefoxDev',
   -- color_scheme = 'Firewatch',
+  -- color_scheme = 'MaterialDark',
   -- color_scheme = 'OceanicMaterial',
   -- color_scheme = 'Thayer Bright',
   -- color_scheme = 'onedark',
+
+  -- warning: `color_scheme` overrides all `colors` options!
+  colors = {
+    foreground = "#cbe1e7",
+    background = "#1e1c31",
+    cursor_bg = "#1e1c31",
+    cursor_border = "#ff6f00",
+    cursor_fg = "#ff6f00",
+    selection_bg = "#cbe1e7",
+    selection_fg = "#1e1c31",
+
+    ansi = { "#141228","#ff5458","#62d196","#ffb378","#65b2ff","#906cff","#63f2f1","#a6b3cc" },
+    brights = { "#565575","#ff8080","#95ffa4","#ffe9aa","#91ddff","#c991e1","#aaffe4","#cbe3e7" },
+  },
 
   adjust_window_size_when_changing_font_size = false,
   bold_brightens_ansi_colors = false,
