b81f75c87221f72db64e8ee8b3a55bcbee8a7791
Tim Cummings <timjc86@gmail.com>
2021-11-14 22:38:18 -0600

    feat: terminal - experiment with wezterm

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+return {
+  -- color_scheme = 'Ayu Mirage',
+  -- color_scheme = 'BlulocoDark',
+  color_scheme = 'ChallengerDeep',
+  -- color_scheme = 'Darkside',
+  -- color_scheme = 'FirefoxDev',
+  -- color_scheme = 'Firewatch',
+  -- color_scheme = 'OceanicMaterial',
+  -- color_scheme = 'Thayer Bright',
+  -- color_scheme = 'onedark',
+
+  bold_brightens_ansi_colors = false,
+  harfbuzz_features = {"calt=0", "clig=0", "liga=0"},
+  hide_tab_bar_if_only_one_tab = true,
+}
