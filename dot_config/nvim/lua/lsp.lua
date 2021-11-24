9626c587856bd2cfdc7bb306ee70c2703f7fe24d
Tim Cummings <timjc86@gmail.com>
2021-11-23 22:37:06 -0600

    feat: vim - add solargraph, ruby language server

--- dot_config/nvim/lua/lsp.lua
+++ dot_config/nvim/lua/lsp.lua
@@ -44,7 +44,7 @@ local handlers =  {
 
 -- Use a loop to conveniently call 'setup' on multiple servers and
 -- map buffer local keybindings when the language server attaches
-local servers = { 'tsserver' }
+local servers = { 'solargraph', 'tsserver' }
 for _, lsp in ipairs(servers) do
   nvim_lsp[lsp].setup {
     on_attach = on_attach,
