9122bbaf78a0c4d52bea65a3bc48f99289ff9e5f
Tim Cummings <timjc86@gmail.com>
2021-12-14 22:53:32 -0600

    feat: terminal - add function to rename tabs

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -41,6 +41,19 @@ for i = 0, 7 do
   })
 end
 
+wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_width)
+  local pane_title = tab.active_pane.title
+  local user_title = tab.active_pane.user_vars.panetitle
+
+  if user_title ~= nil and #user_title > 0 then
+    pane_title = user_title
+  end
+
+  return {
+    {Text=" " .. pane_title .. " "},
+  }
+end)
+
 return {
   -- color_scheme = 'Ayu Mirage',
   -- color_scheme = 'Batman',
