7638d5113c0587f4f0ef3982b1819da18e077e9e
Tim Cummings <timjc86@gmail.com>
2022-04-16 20:31:06 -0500

    refactor: vim - package management
    - move packer bootstrap code from `init.lua` to `plugins.lua`
    - update bootstrap code's syntax with Neovim 0.7 improvements

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,17 +1,5 @@
--- Install packer
-local execute = vim.api.nvim_command
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  execute('!git clone https://github.com/wbthomason/packer.nvim '.. install_path)
-end
-
-local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
-vim.api.nvim_create_autocmd('BufWritePost',
-  { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'plugins.lua' })
-
-require('settings')
 require('plugins')
+require('settings')
 require('mappings')
 require('tabline')
 
--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,3 +1,17 @@
+-- bootstrap packer
+local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  packer_bootstrap = vim.fn.system(
+    { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
+  )
+end
+
+-- recompile plugins when this file changes
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost',
+  { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'plugins.lua' })
+
+-- manage plugins
 return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
   use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
