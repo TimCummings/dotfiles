7a06d32ff1ab1fdc2aab9472bddd22e03b5bd3c9
Tim Cummings <timjc86@gmail.com>
2022-05-01 16:44:47 -0500

    refactor: vim - re-enable treesitter's "ensure installed"
    - and cleanup treesitter setup

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -10,39 +10,8 @@ if pcall(function()
     require('colorizer').setup()
     require('statusline')
     require('nvim-tree').setup()
-
     require('lsp')
     require('treesitter_config').setup()
-    -- installing parsers currently locks up VPS
-    -- manually install parsers one at a time for now with `:TSInstall`
-    -- possible issue match: https://github.com/nvim-treesitter/nvim-treesitter/issues/1254
-    -- require('treesitter_config').setup{
-    --   ensure_installed = {
-    --     "bash",
-    --     "comment",
-    --     "css",
-    --     "dockerfile",
-    --     "go",
-    --     "gomod",
-    --     "graphql",
-    --     "html",
-    --     "javascript",
-    --     "jsdoc",
-    --     "json",
-    --     "lua",
-    --     "python",
-    --     "query",
-    --     "regex",
-    --     "ruby",
-    --     "rust",
-    --     "scss",
-    --     "toml",
-    --     "tsx",
-    --     "typescript",
-    --     "vue",
-    --     "yaml"
-    --   }
-    -- }
   end) then
 else
   print('One or more plugins missing. Run `:PackerInstall`!')
--- dot_config/nvim/lua/treesitter_config.lua
+++ dot_config/nvim/lua/treesitter_config.lua
@@ -2,7 +2,22 @@ local M = {}
 
 function M.setup()
   require('nvim-treesitter.configs').setup{
-    ensure_installed = { 'norg' },
+    ensure_installed = {
+      'bash',
+      'css',
+      'dockerfile',
+      'go',
+      'graphql',
+      'html',
+      'javascript',
+      'json',
+      'lua',
+      'ruby',
+      'rust',
+      'scss',
+      'typescript',
+      'yaml',
+    },
     highlight = {
       enable = true,              -- false will disable the whole extension
     },
