2006ca53d918c813f3bff671a140faaf11a445ca
Tim Cummings <timjc86@gmail.com>
2022-06-03 23:27:51 -0500

    refactor: vim - start orgmode agenda week on Sunday

--- dot_config/nvim/lua/orgmode_config.lua
+++ dot_config/nvim/lua/orgmode_config.lua
@@ -3,6 +3,7 @@ local M = {}
 function M.setup()
   require('orgmode').setup({
     org_agenda_files = { '~/Documents/org/*' },
+    org_agenda_start_on_weekday = 7,
     org_default_notes_file = '~/Documents/org/refile.org',
     win_split_mode = 'float',
   })
