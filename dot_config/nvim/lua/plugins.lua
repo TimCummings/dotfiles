16153bc9d2ab4e6d91d98dcec262ac3556004f32
Tim Cummings <timjc86@gmail.com>
2022-12-18 18:18:22 -0600

    fix: vim - bootstrap packer
    the final step to actually install plugins was missing; also cleaned up code with updates from packer's README

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -1,10 +1,17 @@
 -- bootstrap packer
-local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
-if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  packer_bootstrap = vim.fn.system(
-    { 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path }
-  )
+local ensure_packer = function()
+  local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
+
+  if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+    vim.fn.system({ 'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path })
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+
+  return false
 end
+
+local packer_bootstrap = ensure_packer()
 
 -- recompile plugins when this file changes
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
@@ -122,4 +129,11 @@ return require('packer').startup(function(use)
     'iamcco/markdown-preview.nvim',
     run = function() vim.fn['mkdp#util#install']() end,
   }
+
+  -- continuation of bootstrap process:
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
 end)
