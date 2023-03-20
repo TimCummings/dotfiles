6995cb38f0d35dddce6b827bc5204e0338b97e1d
Tim Cummings <timjc86@gmail.com>
2023-03-20 04:20:46 -0500

    refactor: nvim - adjustments to lazy loading for some plugins

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,11 +1,11 @@
 -- list of plugins that don't need much special configuration
 return {
   'tpope/vim-surround',
-  { 'tpope/vim-repeat', keys = '.', },
+  'tpope/vim-repeat',
   { 'tpope/vim-fugitive', cmd = 'G', },
   { 'junegunn/goyo.vim', cmd = 'Goyo', },
   { 'junegunn/limelight.vim', cmd = 'Goyo', },
-  { 'dhruvasagar/vim-table-mode', keys = '<Leader>tm' },
+  { 'dhruvasagar/vim-table-mode', cmd = { 'TableModeEnable', 'TableModeToggle' }, keys = '<Leader>tm' },
 
   {
     'numToStr/Comment.nvim',
