b0a9c1666886444acba15b62901a49b8747647e3
Tim Cummings <timjc86@gmail.com>
2023-05-28 15:56:06 -0500

    refactor: scripts - improve dependency handling
    - add several missing dependencies to list of packages/plugins to install
    - reorder installations so that dependencies are installed first
    - document major dependencies

--- scripts/non_package_install.sh
+++ scripts/non_package_install.sh
@@ -3,16 +3,40 @@
 # this should be run from bootstrap.sh;
 # be careful about running this directly without first verifying dependencies
 
+# DEPENDENCY NOTICES:
+# - Python depends on libbz2-dev, libncurses-dev, libreadline-dev, libsqlite3-dev, python3-tk,
+#   liblzma-dev
+# - Ruby dependso on libyaml (libyaml-dev), libssl-dev
+# - Tmux depends on a bunch of stuff! Install it with OS's package manager (apt),
+#   or instally it manually, fixing dependency errors one at a time.
+# - yamllint depends on jq and Python3
+
 RED=$(tput setaf 1)
 GREEN=$(tput setaf 2)
 # YELLOW=$(tput setaf 3)
 NONE=$(tput sgr0)
 
-apt_packages="tldr tmux"
-asdf_plugins="direnv fd fzf golang lazygit neovim nodejs ripgrep shellcheck yarn"
+apt_packages="jq libbz2-dev liblzma-dev libncurses-dev libreadline-dev libsqlite3-dev libssl-dev libyaml-dev python3-tk tldr tmux"
+asdf_plugins="actionlint direnv fd fzf golang lazygit neovim nodejs ripgrep shellcheck yarn"
 
 echo
 echo "Starting non-packages installation..."
+
+########## apt installs ##########
+echo
+echo -e "    apt packages..."
+for package in $apt_packages; do
+  printf '\t%-20s' "${package}..."
+  if type "$package" &> /dev/null; then
+    printf '    %-10s\n' "${GREEN}found!${NONE}"
+  else
+    printf '    %-10s\n' "${RED}missing!${NONE}"
+    echo "Installing ${package}..."
+    sudo apt install "$package"
+  fi
+done
+
+
 
 ########## ASDF ##########
 # Periodically check the version number below and update it!
@@ -45,23 +69,7 @@ for plugin in $asdf_plugins; do
 done
 
 # these take a long time to install; do them manually:
-# slow_asdf_plugins = "python ruby"
-
-
-
-########## apt installs ##########
-echo
-echo -e "    apt packages..."
-for package in $apt_packages; do
-  printf '\t%-20s' "${package}..."
-  if type "$package" &> /dev/null; then
-    printf '    %-10s\n' "${GREEN}found!${NONE}"
-  else
-    printf '    %-10s\n' "${RED}missing!${NONE}"
-    echo "Installing ${package}..."
-    sudo apt install "$package"
-  fi
-done
+# slow_asdf_plugins = "python ruby rubocop yamllint"
 
 
 
@@ -76,6 +84,9 @@ else
   echo "Installing starship prompt..."
   sh -c "$(curl -fsSL https://starship.rs/install.sh)"
 fi
+
+printf '\t%-20s' "Installing ESlint..."
+yarn global add eslint
 
 
 
