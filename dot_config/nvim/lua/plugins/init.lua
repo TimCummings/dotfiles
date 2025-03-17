f156faa37e1dda4a545eaeac49071a558949358a
Tim Cummings <timjc86@gmail.com>
2025-03-17 10:14:57 -0500

    feat: vim - add chezmoi template syntax highlighting plugin

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -29,6 +29,14 @@ return {
     version = false
   },
 
+  { -- syntax highlighting in chezmoi templates
+    'alker0/chezmoi.vim',
+    lazy = false,
+    init = function()
+      vim.g['chezmoi#use_tmp_buffer'] = true
+    end,
+  },
+
   {
     'iamcco/markdown-preview.nvim',
     config = function()
--- dot_config/nvim/lua/plugins/treesitter.lua
+++ dot_config/nvim/lua/plugins/treesitter.lua
@@ -28,6 +28,13 @@ return {
         },
 
         highlight = {
+          -- chezmoi.vim template syntax highlighting is via Vim builtin, not treesitter
+          disable = function()
+            -- check if 'filetype' option includes 'chezmoitmpl'
+            if string.find(vim.bo.filetype, 'chezmoitmpl') then
+              return true
+            end
+          end,
           enable = true,              -- false will disable the whole extension
         },
 
