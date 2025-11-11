1b001741967908220dc9c5a4fe5ef7a67c68235b
Tim Cummings <timjc86@gmail.com>
2025-11-10 23:21:49 -0600

    style: vim - render markdown visual improvements
    - remove most virtual space to prevent stutter when scrolling
    - make code block labels less obtrusive
    - tone down the most vivid highlights

--- dot_config/nvim/lua/plugins/render_markdown.lua
+++ dot_config/nvim/lua/plugins/render_markdown.lua
@@ -4,15 +4,31 @@ return {
   dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
   opts = {
     bullet = {
+      hightlight = 'FloatBorder',
       icons = { '•', '-', '∘' },
     },
 
+    code = {
+      conceal_delimiters = false,
+      highlight_language = 'Comment',
+      language_icon = false,
+    },
+
+    dash = {
+      highlight = 'FloatBorder',
+    },
+
     heading = {
-      above = '▁',
-      below = '▔',
-      border = true,
-      border_virtual = true,
+      -- above = '▁',
+      -- below = '▔',
+      border = false,
+      -- border_virtual = true,
       icons = { '◆ ', '◇ ', '● ', '◯ ', '▶ ', '▷ ' },
+    },
+
+    pipe_table = {
+      head = 'FloatBorder',
+      row = 'FloatBorder',
     },
 
     sign = { enabled = false },
