279d958817ec2290a4424fb30e0195366dfe695f
Tim Cummings <timjc86@gmail.com>
2026-07-04 20:27:31 -0500

    chore: remove fish shell
    Fish is great. The out-of-the-box user experience is top notch. But
    losing POSIX compliance is inconvenient, and it doesn't always play
    nicely with things like direnv without special handling. I've already
    spent the time to get ZSH about where Fish is OOTB, and maintaining
    POSIX compliance is worth it in my current opinion.

--- scripts/setup_dependencies.sh
+++ scripts/setup_dependencies.sh
@@ -8,13 +8,6 @@ NONE='\033[0m'
 echo
 echo "Checking dependencies..."
 
-# echo -e "\tchezmoi..."
-# if ! type "${HOME}/.local/bin/chezmoi" &> /dev/null; then
-#   echo -e "\t\t${YELLOW}Chezmoi not found. Installing...${NONE}\n"
-#   sh -c "$(curl -fsLS get.chezmoi.io/lb)" -- init --apply git@github.com:TimCummings/dotfiles.git
-#   echo -e "\t\t${GREEN}Done!${NONE}"
-# fi
-
 echo -e "\tzsh..."
 if type zsh &> /dev/null; then
   echo -e "\t\t${GREEN}Found!${NONE}"
@@ -28,12 +21,3 @@ if [[ "$(basename "$SHELL")" != "zsh" ]]; then
   echo -e "\t\t${YELLOW}Zsh not default login shell. Setting...${NONE}"
   sudo chsh -s "$(command -v zsh)" "$(whoami)"
 fi
-
-# echo -en "\tfish?"
-# if type fish &> /dev/null; then
-#   echo -e "\t\t\t${GREEN}found!${NONE}"
-# else
-#   echo -e "\t\t\t${RED}missing!${NONE}"
-#   echo "Install fish, then try again."
-#   exit 1
-# fi
